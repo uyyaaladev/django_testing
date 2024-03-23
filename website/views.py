a18aff92ac79ce10a9268d2cd27bd86db5bd589d
yeshwanth sai <yashwanthdataentryworks@gmail.com>
2024-03-23 18:42:03 +0530

    second commit

--- website/views.py
+++ website/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
 # Create your views here.
+
+
+def home(request):
+    # return HttpResponse("Hello, world. You're at the myapp home page.")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in successfully")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect("home")
+    else:
+        return render(request, "home.html")
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logged out successfully")
+    return redirect("home")
